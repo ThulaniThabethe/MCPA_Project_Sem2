1d8e8425379abe633cc3778db7d233d324d8781a
Thulani Thabethe <163007950+ThulaniThabethe@users.noreply.github.com>
2025-09-19 19:32:41 +0200

    full wireframes upload
    full wireframes upload

---
+++ app/src/main/java/com/example/mcpa_project_sem22/StoreLocatorActivity.java
@@ -0,0 +1,14 @@
+package com.example.mcpa_project_sem22;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StoreLocatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_locator);
+    }
+}
